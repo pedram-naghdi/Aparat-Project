408e125d1597a2d9a0bc11d9f76f0218519845ca
pedram-naghdi <pedram.naghdi@gmail.com>
2023-12-12 15:38:12 +0330

    header widths

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -1,10 +1,16 @@
 const Header = () => {
     return (
         <header className="py-3">
-            <div className="container mx-auto">
-                <a href="/" title="آپارات">
+            <div className="container mx-auto flex gap-2">
+                <a href="/" title="آپارات" className="w-3/12">
                     <img src="assets/img/logo.png" alt="آپارات" className="logo" />
                 </a>
+                <form className="w-5/12">
+                    <input type="text" placeholder="جستجو در فیلم ها" className="w-full"/>
+                </form>
+                <div className="w-4/12">
+
+                </div>
             </div>
         </header>
     )
