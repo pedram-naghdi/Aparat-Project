c4ebff64782d416e849add36c93b157c2b272f93
pedram-naghdi <pedram.naghdi@gmail.com>
2023-12-12 15:17:12 +0330

    Create Layout

---
+++ components/footer/page.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="py-3  bg-gray-700">
+            <div className="container mx-auto text-center text-white text-sm">
+                <p>تمامی حقوق برای آپارات محفوظ است.</p>
+            </div>
+        </footer>
+    )
+}
+
+export  default Footer
---
+++ components/header/page.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <header className="py-3">
+            <div className="container mx-auto">
+                <a href="/" title="آپارات">
+                    <img src="assets/img/logo.png" alt="آپارات" className="logo" />
+                </a>
+            </div>
+        </header>
+    )
+}
+
+export  default Header
