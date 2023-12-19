3fb7f7515b4a06e1d0a7f406d3546d9bd133a36b
pedram-naghdi <pedram.naghdi@gmail.com>
2023-12-19 12:25:13 +0330

    add relative Videos

--- components/video/allvideos.tsx
+++ components/video/allvideos.tsx
@@ -7,46 +7,56 @@ const AllVideos = () => {
     const [videos , setVideos] = useState([])
    
     useEffect(() => {
-        axios({
-            method: 'post',
-            url: 'https://www.aparat.com/etc/api/videoByUser/username/zoomit/perpage/16'
+        async function GetAllVideos() {
+            await axios({
+                method: 'post',
+                url: 'https://www.aparat.com/etc/api/videoByUser/username/zoomit/perpage/16'
+    
+            }).then((response) => {
+                setVideos(response.data.videobyuser)
+            }, (error) => {
+                console.log(error)
+            })
+            console.log("1")
+        }
 
-        }).then((response) => {
-            setVideos(response.data.videobyuser)
-        }, (error) => {
-            console.log(error)
-        })
+        GetAllVideos()
+
       }, []);
+
+      console.log("2")
 
     return ( 
         <>
         <div className="page-title flex items-center gap-1 mb-5">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-blue-600 stroke-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-blue-600 stroke-2">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0118 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0118 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 016 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5" />
             </svg>
             <h1 className=" text-md font-bold">ویدئوها</h1>
         </div>
-        <div className="videos flex flex-wrap">
+        <div className="videos flex flex-wrap mb-8">
             {
             videos.map((item) => (
-            <div className="video w-full md:w-[50%] lg:w-[25%] px-4 pb-8" key={item.id}>
-                <a href={`/video/${item.uid}`} >
-                    <div className="video-img">
-                        <img src={item.small_poster} className="w-full rounded-[5px]" />
-                    </div>
-                    <h2>{item.title}</h2>
-                    <div className="more-data flex items-center justify-between text-[10px] text-blue-600">
-                        <span className="visit flex items-center gap-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                            </svg>{item.visit_cnt}</span>
-                        <span className="date flex items-center gap-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
-                            </svg>{item.sdate}</span>
-                    </div>
-                </a>
+            <div className="video w-full md:w-[50%] lg:w-[25%] px-2 pb-5" key={item.id}>
+                <div className="px-2 pt-2 pb-3 rounded-[5px] hover:bg-blue-50">
+                    <a href={`/video/${item.uid}`} >
+                        <div className="video-img">
+                            <img src={item.small_poster} className="w-full rounded-[5px]" />
+                        </div>
+                        <h2>{item.title}</h2>
+                        <div className="more-data flex items-center justify-between text-[10px] text-blue-600">
+                            <span className="visit flex items-center gap-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>{item.visit_cnt}</span>
+                            <span className="date flex items-center gap-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                                </svg>{item.sdate}</span>
+                        </div>
+                    </a>
+                </div>
             </div>
             ))
             }
--- components/video/videoDetails.tsx
+++ components/video/videoDetails.tsx
@@ -1,15 +1,21 @@
 "use client"
+
+import { useAtom } from 'jotai'
+import {jotaiOpenSidebar} from '@/app/jotai'
 import { useState, useEffect } from "react"
 import axios from "axios"
 import RelativeVideos from "./RelativeVideos"
 
 const VideoDetails = ({videoID}:any) => {
 
+    const [ , setOpenSidebar] = useAtom<boolean>(jotaiOpenSidebar)
+    
     const [video , setVideo] = useState<any>([])
     const [tags , setTags] = useState<any>([])
  
-    useEffect(() => {
-        
+    useEffect(() => {   
+
+        setOpenSidebar(false)
 
         async function getVideo() {
             await axios({
@@ -18,7 +24,8 @@ const VideoDetails = ({videoID}:any) => {
 
             }).then((response) => {
                 setVideo(response.data.video)
-                setTags(response.data.video.tags)
+                const selectedTags = response.data.video.tags.slice(0, 8);
+                setTags(selectedTags)
             }, (error) => {
                 console.log(error)
             })
@@ -26,22 +33,22 @@ const VideoDetails = ({videoID}:any) => {
 
         getVideo()
 
-      }, [])
+      }, [videoID])
 
       console.log(video)
       console.log(tags)
 
     return ( 
-        <div className="flex">
-            <div className="video w-full md:w-[calc(100%-300px)]">
-                <div className="video w-full px-4 pb-8">
+        <div className="flex lg:flex-row flex-col">
+            <div className="video w-full lg:w-[calc(100%-300px)]">
+                <div className="video px-4 pb-8">
                     <iframe src={video.frame} width="100%" height="430px"></iframe>
                     <div className="flex items-start gap-2 justify-between my-5 flex-col md:flex-row md:items-center md:gap-5">
                         <h2 className="font-bold">{video.title}</h2>
                         <div className="likes flex items-center justify-between gap-3">
                             <span className="flex items-center justify-between text-[12px] gap-1">
                                 {video.visit_cnt}
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-green-900">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                 </svg>
@@ -54,7 +61,7 @@ const VideoDetails = ({videoID}:any) => {
                             </span>
                             <span className="flex items-center justify-between text-[12px] gap-1">
                                 K<span>{Math.round(video.size / 1024)}</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 text-blue-600">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0l-3-3m3 3l3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
                                 </svg>
                             </span>
@@ -63,23 +70,25 @@ const VideoDetails = ({videoID}:any) => {
                     <div className="description text-gray-600 text-sm leading-8 text-justify">
                         <p>{video.description}</p>
                     </div>
-                    <div className="tags flex items-center justify-start mt-5 text-gray-500 text-[11px]">
-                        <span className="date flex items-center gap-1 ml-5">
+                    <div className="tags flex mt-5 text-gray-500 text-[11px] flex-col md:flex-row gap-3">
+                        <span className="date flex items-start md:items-center gap-1">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
                         </svg>
                         {video.sdate}
                         </span>
-                        {   
-                        tags.map((tag:any) => (
-                            <a href={`tag/${tag.name.replace(/ /g,"_")}`}>
-                                <span className="ml-3 hover:text-blue-600">#{tag.name}</span>
-                            </a>))
-                        }
+                        <div className="tags">
+                            {
+                            tags.map((tag:any) => (
+                                <a href={`tag/${tag.name.replace(/ /g,"_")}`} key={tag.cnt}>
+                                    <span className="ml-3 hover:text-blue-600">#{tag.name}</span>
+                                </a>))
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="relative-videos w-full md:w-[300px] pr-3">
+            <div className="relative-videos w-full lg:w-[300px] pr-3">
                 <RelativeVideos catID={parseInt(video.cat_id)} />
             </div>
         </div>
